da737f1a3e394f4f59c2b286a9ca47d408f9f976
+ <awino.millicent@student.moringaschool.com>
2022-08-17 16:13:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+if (feet <= 400){
+  return 'This one is on me!';
+  }
+  else if(feet>=2000){
+
+    if (feet >=2500){
+      return 'No can do.';
+    }
+    return'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  return (destination==='NYC'?'Ok, sounds good.':"No go.")
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+   }
+  }
